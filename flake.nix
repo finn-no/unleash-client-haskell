a8d20407d2312d22418ecc966918d7ede0209ca5
Even Brenden <evenbrenden@gmail.com>
2023-01-24 12:55:58 +0100

    Use unleash-client-haskell-core at 0.6.5 tag

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     unleash-client-haskell-core.url =
-      "github:finn-no/unleash-client-haskell-core";
+      "github:finn-no/unleash-client-haskell-core?ref=0.6.5";
   };
   outputs =
     { self, nixpkgs, flake-compat, flake-utils, unleash-client-haskell-core }:
