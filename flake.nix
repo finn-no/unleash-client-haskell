42cd35ad659018e45b5d175c74a72d28920dc5f2
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-12 08:56:39 +0100

    Reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -9,8 +9,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     unleash-client-haskell-core.url = "github:finn-no/unleash-client-haskell-core?ref=0.10.0";
   };
-  outputs =
-    { self, nixpkgs, flake-compat, flake-utils, unleash-client-haskell-core }:
+  outputs = { self, nixpkgs, flake-compat, flake-utils, unleash-client-haskell-core }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -19,17 +18,16 @@
             (self: super: {
               haskellPackages = super.haskellPackages.override {
                 overrides = self: super: {
-                  unleash-client-haskell-core = pkgs.haskell.lib.dontCheck
-                    unleash-client-haskell-core.defaultPackage.${system};
+                  unleash-client-haskell-core =
+                    pkgs.haskell.lib.dontCheck unleash-client-haskell-core.defaultPackage.${system};
                 };
               };
             })
           ];
         };
-        unleash-client-haskell =
-          pkgs.haskellPackages.callCabal2nix "unleash-client-haskell" ./. { };
+        unleash-client-haskell = pkgs.haskellPackages.callCabal2nix "unleash-client-haskell" ./. { };
       in {
-        defaultPackage = unleash-client-haskell; # nix build .?submodules=1
+        defaultPackage = unleash-client-haskell;
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             cabal-install
