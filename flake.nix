0498877d5d15d811f0c6de71f1e97c189bb90103
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-14 09:04:53 +0100

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -7,24 +7,11 @@
     };
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    unleash-client-haskell-core.url = "github:finn-no/unleash-client-haskell-core?ref=0.10.0";
   };
-  outputs = { self, nixpkgs, flake-compat, flake-utils, unleash-client-haskell-core }:
+  outputs = { self, nixpkgs, flake-compat, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: {
-              haskellPackages = super.haskellPackages.override {
-                overrides = self: super: {
-                  unleash-client-haskell-core =
-                    pkgs.haskell.lib.dontCheck unleash-client-haskell-core.defaultPackage.${system};
-                };
-              };
-            })
-          ];
-        };
+        pkgs = import nixpkgs { inherit system; };
         unleash-client-haskell = pkgs.haskellPackages.callCabal2nix "unleash-client-haskell" ./. { };
       in {
         defaultPackage = unleash-client-haskell;
